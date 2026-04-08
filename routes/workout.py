144ab9bf3f3ac4305ac377e450eec988c6e0b5bf
Martí Busquets <marti.busque@gmail.com>
2026-04-08 18:42:49 +0200

    resend_by_date

--- routes/workout.py
+++ routes/workout.py
@@ -1,4 +1,10 @@
+import asyncio
+import json
+import os.path
+
 from fastapi import APIRouter
+
+from config.http_client import HttpClient
 from models.doymos_reader import bike_reader
 
 router = APIRouter(prefix="/workout", tags=["workout"])
@@ -20,3 +26,14 @@ def set_active_stages():
 def set_deactivate_stages():
     bike_reader.state.active_stages = False
     return {"start_date": bike_reader.start_date, "data": bike_reader.state}
+
+
+@router.get("/resend/{date}")
+async def resend_by_date(date):
+    path = f"files/session_{date[0:4]}_{date[4:6]}_{date[6:8]}.json"
+    if os.path.exists(path):
+        with open(path, "r") as f:
+            data = json.load(f)
+            await HttpClient.end_bike_session(data)
+            return data
+    return f"File {path} do not exist"
